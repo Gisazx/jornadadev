c2586b6ed95be017ffc4cf2a4de3e9c70ebd7e16
Gisazx <gisellys930@gmail.com>
2023-03-30 17:53:13 -0300

    Video adicionado

---
+++ src/pages/Video.js
@@ -0,0 +1,36 @@
+import React, { useRef, useState } from 'react'
+import "./video.css"
+
+function Video() {
+
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+
+    function handleStart(){
+
+        if(play){
+            videoRef.current.pause()
+            setPlay(false)
+        } else {
+            videoRef.current.play()
+            setPlay(true)
+        }
+        
+    }
+
+  return (
+    <div className="video">
+        <video 
+        className="video__player"
+        ref={videoRef}
+        onClick={handleStart}
+        loop
+        src={require("../assets/brecker2.mp4")}
+        >
+
+        </video>
+    </div>
+  )
+}
+
+export default Video
